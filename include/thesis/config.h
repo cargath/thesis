3de91047fb94859754c87aac010dc54daba5800a
Carsten Könemann <cargath@informatik.uni-bremen.de>
2013-06-16 17:09:39 +0200

    Cleanup: Removed some unused code

--- include/thesis/config.h
+++ include/thesis/config.h
@@ -13,8 +13,4 @@ static const std::string   CAMERA_FRAME = "/head_mount_kinect_rgb_optical_frame"
 static const std::string   MAP_FRAME    = "/map";
 static const ros::Duration TF_TIMEOUT   = ros::Duration(5.0);
 
-// KeyPoint clustering
-static const unsigned int DBSCAN_MIN_POINTS   = 5;
-static const unsigned int DBSCAN_MAX_DISTANCE = 25;
-
 #endif //__CONFIG__
